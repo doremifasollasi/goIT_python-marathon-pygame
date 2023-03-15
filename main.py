0d59f34efed99cb405f8a7549d932b6b5c6769c1
doremifasollasi <lena.lenova.2014@gmail.com>
2023-03-15 20:03:09 +0200

    Перша версія проекту

---
+++ main.py
@@ -0,0 +1,42 @@
+import pygame
+from pygame.constants import QUIT
+
+pygame.init()
+
+screen = width, height = 800, 600
+
+BLACK = 0, 0, 0
+WHITE = 255, 255, 255
+RED = 255, 0, 0
+GREEN = 0, 255, 0
+
+main_surface = pygame.display.set_mode(screen)
+
+ball = pygame.Surface((20, 20))
+ball.fill(WHITE)
+ball_rect = ball.get_rect()
+ball_speed = [1,1]
+
+is_working = True
+
+while is_working:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            is_working = False  # pygame.quit()
+    ball_rect = ball_rect.move(ball_speed)
+    
+    if ball_rect.bottom >= height or ball_rect.top <= 0: # відбивання м'яча від нижньої та верхньої межі
+        ball.fill((RED))
+        ball_speed[1] = -ball_speed[1]
+
+    if ball_rect.right >= width or ball_rect.left <= 0: # відбивання м'яча від лівого та правого краю
+        ball.fill((GREEN))
+        ball_speed[0] = -ball_speed[0]
+    
+    main_surface.fill(BLACK)
+
+    main_surface.blit(ball, ball_rect)
+
+    
+    # main_surface.fill((155,155,155))
+    pygame.display.flip()
