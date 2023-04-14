864a3131895656a3abbdf6edfbe6bab997ad98dc
doremifasollasi <lena.lenova.2014@gmail.com>
2023-04-15 00:25:38 +0300

    Added hitting the ball from the right/left side of the playing field with a change of color

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ main_surface = pygame.display.set_mode(screen)
 
 ball = pygame.Surface((20, 20))
 ball.fill(WHITE)
-ball_rect = ball.get_rect()
+ball_rect = ball.get_rect() #координати положення ball - початкове(0,0)
 ball_speed = [1,1]
 
 is_working = True
@@ -24,7 +24,9 @@ while is_working:
             is_working = False  # pygame.quit()
     ball_rect = ball_rect.move(ball_speed)
     
-    
+    if ball_rect.bottom >= height or ball_rect.top <= 0: #відбивання м'яча від верхньої та нижньої сторони ігрового поля зі зміною кольору
+        ball.fill((255, 0, 0))
+        ball_speed[1] = -ball_speed[1]
     
     main_surface.fill(BLACK)
 
