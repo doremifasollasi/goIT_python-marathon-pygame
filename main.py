49e4869eebfd78ec5143c49011ece14695f81485
doremifasollasi <lena.lenova.2014@gmail.com>
2023-04-15 03:59:55 +0300

    Added functionality for the goose to move

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import random
+from os import listdir # повертає список всіх файлів, що є у теці
 
 import pygame
 from pygame.constants import QUIT, K_DOWN, K_UP, K_RIGHT, K_LEFT
@@ -19,9 +20,12 @@ font = pygame.font.SysFont('Verdana', 20)
 
 main_surface = pygame.display.set_mode(screen)
 
+IMGS_PATH = 'goose'
+
 # player = pygame.Surface((20, 20))
 # player.fill(WHITE)
-# player_imgs = [pygame.image.load(file).convert_alpha() for file in]
+player_imgs = [pygame.image.load(IMGS_PATH + '/' + file).convert_alpha() for file in listdir(IMGS_PATH)]
+player = player_imgs[0]
 player = pygame.image.load('player.png').convert_alpha()
 player_rect = player.get_rect() #координати положення player - початкове(0,0)
 player_speed = 10
@@ -46,10 +50,15 @@ bgX2 = bg.get_width()
 bg_speed = 3
 
 CREATE_ENEMY = pygame.USEREVENT + 1
-pygame.time.set_timer(CREATE_ENEMY, 1500)
+pygame.time.set_timer(CREATE_ENEMY, 2500)
 
 CREATE_BONUS = pygame.USEREVENT + 2
-pygame.time.set_timer(CREATE_BONUS, 2500)
+pygame.time.set_timer(CREATE_BONUS, 3500)
+
+CHANGE_IMG = pygame.USEREVENT + 3
+pygame.time.set_timer(CHANGE_IMG, 125)
+
+img_index = 0
 
 scores = 0
 
@@ -71,6 +80,14 @@ while is_working:
         
         if event.type == CREATE_BONUS:
             bonuses.append(create_bonus())
+
+        if event.type == CHANGE_IMG:
+            img_index += 1
+            if img_index == len(player_imgs):
+                img_index = 0
+            player = player_imgs[img_index]
+
+
     # player_rect = player_rect.move(player_speed) перенесли в команду K_DOWN
     
     # if player_rect.bottom >= height or player_rect.top <= 0: #відбивання м'яча від верхньої та нижньої сторони ігрового поля зі зміною кольору
@@ -81,7 +98,7 @@ while is_working:
     #     player.fill((GREEN))
     #     player_speed[1] = -player_speed[1]
 
-    pressed_key = pygame.key.get_pressed()
+    pressed_keys = pygame.key.get_pressed()
 
     # main_surface.fill(WHITE)
     # main_surface.blit(bg, (0,0))
@@ -90,17 +107,17 @@ while is_working:
     bgX2 -= bg_speed
 
     if bgX < -bg.get_width():
-        bgX = bg.get_width
+        bgX = bg.get_width()
 
     if bgX2 < -bg.get_width():
-        bgX2 = bg.get_width
+        bgX2 = bg.get_width()
 
-    main_surface.blit(bg, (bgX, 0))
+    main_surface.blit(bg, (bgX, 0)) 
     main_surface.blit(bg, (bgX2, 0))
 
     main_surface.blit(player, player_rect)
 
-    main_surface.blit(font.render(str(scores), True, WHITE), (width-30, 0)) # відмальовуємо рахунок гри
+    main_surface.blit(font.render(str(scores), True, WHITE), (width - 30, 0)) # відмальовуємо рахунок гри
 
     for enemy in enemies:
         enemy[1] = enemy[1].move(-enemy[2],0)
@@ -120,21 +137,21 @@ while is_working:
 
         if bonus[1].bottom >= height: # Проблема: кількість ворогів у списку буде постійно збільшуватись, що призведе до навантаження на пам'ять. Рішення: видалення. Реалізація: Якщо позиція нашого ворога більша за 0, то ми його видяляємо.
             bonuses.pop(bonuses.index(bonus))
-            scores += 1
 
         if player_rect.colliderect(bonus[1]):
             bonuses.pop(bonuses.index(bonus))
+            scores += 1
 
-    if pressed_key[K_DOWN] and not player_rect.bottom >= height: #додаємо керування клавіши ВНИЗ
+    if pressed_keys[K_DOWN] and not player_rect.bottom >= height: #додаємо керування клавіши ВНИЗ
         player_rect = player_rect.move(0, player_speed)
 
-    if pressed_key[K_UP] and not player_rect.top <= 0: #додаємо керування клавіши ДОГОРИ
+    if pressed_keys[K_UP] and not player_rect.top <= 0: #додаємо керування клавіши ДОГОРИ
         player_rect = player_rect.move(0, -player_speed)
 
-    if pressed_key[K_RIGHT] and not player_rect.right >= width: #додаємо керування клавіши ПРАВОРУЧ
+    if pressed_keys[K_RIGHT] and not player_rect.right >= width: #додаємо керування клавіши ПРАВОРУЧ
         player_rect = player_rect.move(player_speed, 0)
 
-    if pressed_key[K_LEFT] and not player_rect.left <= 0: #додаємо керування клавіши ЛІВОРУЧ
+    if pressed_keys[K_LEFT] and not player_rect.left <= 0: #додаємо керування клавіши ЛІВОРУЧ
         player_rect = player_rect.move(-player_speed, 0)
     
     # main_surface.fill((155,155,155))
